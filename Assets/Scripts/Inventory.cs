e38089f1efc9c8d25280d2aa9a4ba65ce6a7993d
dotunade@mit.edu <dotunade@mit.edu>
2019-03-03 14:37:43 -0500

    Create Sprite Folders and Inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> characterItems = new List<Item>();
+    public ItemDatabase database;
+
+    public void GiveItem(int id)
+    {
+        Item newItem = database.GetItem(id);
+        this.characterItems.Add(newItem);
+        Debug.Log("Gave the rocket: " + newItem.title);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GiveItem(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
