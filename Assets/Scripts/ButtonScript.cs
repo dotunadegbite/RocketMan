b889f02a597d000cac2da387bf5c0db7acf7fa14
Jini Gabbidon <jini@mit.edu>
2019-03-04 15:53:53 -0500

    Inventory and shop added but not functional.

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public Button buttonComponent;
+    public Text nameLabel;
+    public Image iconImage;
+    //public Text priceText;
+
+
+    private Item item;
+    private ContentListManager scrollList;
+
+    // Use this for initialization
+    void Start()
+    {
+        buttonComponent.onClick.AddListener(HandleClick);
+    }
+
+    public void Setup(Item currentItem, ContentListManager currentScrollList)
+    {
+        item = currentItem;
+        nameLabel.text = item.title;
+        iconImage.sprite = item.icon;
+        //priceText.text = item.price.ToString();
+        scrollList = currentScrollList;
+
+    }
+
+    public void HandleClick()
+    {
+        scrollList.TryTransferItemToOtherShop(item);
+    }
+}
---
+++ Assets/Scripts/ContentListManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class ContentListManager : MonoBehaviour
+{
+
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public ContentListManager otherShop;
+    //public Text myGoldDisplay;
+    public ObjectPool buttonObjectPool;
+
+    public float gold = 20f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        RefreshDisplay();
+    }
+
+    void RefreshDisplay()
+    {
+        //myGoldDisplay.text = "Gold: " + gold.ToString();
+        RemoveButtons();
+        AddButtons();
+    }
+
+    private void RemoveButtons()
+    {
+        while (contentPanel.childCount > 0)
+        {
+            GameObject toRemove = transform.GetChild(0).gameObject;
+            buttonObjectPool.ReturnObject(toRemove);
+        }
+    }
+
+    private void AddButtons()
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            Item item = itemList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+            newButton.transform.SetParent(contentPanel);
+
+            ButtonScript sampleButton = newButton.GetComponent<ButtonScript>();
+            sampleButton.Setup(item, this);
+        }
+    }
+
+    public void TryTransferItemToOtherShop(Item item)
+    {
+        /**
+         * Commented out becuase Items currently have no price
+         */
+        //if (otherShop.gold >= item.price)
+        //{
+        //    gold += item.price;
+        //    otherShop.gold -= item.price;
+
+        //    AddItem(item, otherShop);
+        //    RemoveItem(item, this);
+
+        //    RefreshDisplay();
+        //    otherShop.RefreshDisplay();
+        //    Debug.Log("enough gold");
+
+        //}
+        Debug.Log("attempted");
+    }
+
+    void AddItem(Item itemToAdd, ContentListManager shopList)
+    {
+        shopList.itemList.Add(itemToAdd);
+    }
+
+    private void RemoveItem(Item itemToRemove, ContentListManager shopList)
+    {
+        for (int i = shopList.itemList.Count - 1; i >= 0; i--)
+        {
+            if (shopList.itemList[i] == itemToRemove)
+            {
+                shopList.itemList.RemoveAt(i);
+            }
+        }
+    }
+}
