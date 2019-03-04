db3be8e30935ac3d82dce55d6d4eb2735aa0d339
casparmkerr <casparmkerr@gmail.com>
2019-03-04 14:50:09 -0500

    Added color changing background

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,8 +8,23 @@ public class CameraController : MonoBehaviour
     public float smoothTime = 1F;
     private Vector3 velocity = Vector3.zero;
 
+    private float startTime;
+    public float GameDuration;
+    private Color backgroundcolor;
+    private float H, S, V, originalV;
+
+    public Camera cam;
+
     void Start()
     {
+        H = 0.5527777778f;
+        S = 0.69f;
+        originalV = 0.96f;
+        //V = originalV;
+        backgroundcolor = Color.HSVToRGB(H, S, V);
+        startTime = Time.time;
+        cam = GetComponent<Camera>();
+        cam.clearFlags = CameraClearFlags.SolidColor;
         Debug.Log("camera online");
     }
 
@@ -22,5 +37,13 @@ public class CameraController : MonoBehaviour
             rocket.transform.position - new Vector3(0, 0, 10), ref velocity, smoothTime);
         transform.position = new Vector3(smoothedTransform.x, rocket.transform.position.y, transform.position.z);
         Debug.Log(transform.position);
+        changeBackgroundColor();
+    }
+
+    void changeBackgroundColor()
+    {
+        V = (1-(Time.time - startTime) / GameDuration)*originalV;
+        backgroundcolor = Color.HSVToRGB(H, S, V);
+        cam.backgroundColor = backgroundcolor;
     }
 }
