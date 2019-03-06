22aabecc9026c149f676eb9e90a3aa19e5958043
Severyn Kozak <severyn.kozak@gmail.com>
2019-03-06 15:34:41 -0500

    Add meta files

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RocketController : MonoBehaviour
 {
     List<Stage> stages;
-    double posX, posY, velX, velY, accX, accY;  
+    double posX, posY, velX, velY, accX, accY;
+    bool flying;
+    double targetHeight;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("rocket starting");
+        flying = false;
         stages = new List<Stage>();
         stages.Add(new Stage(0, "stage 1", "stage 1", null, null, 100, 10.0D / 60, 100, 100, 1000, 1));
     }
@@ -26,13 +31,30 @@ public class RocketController : MonoBehaviour
         {
             posX += 0.1;
         }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            startRocket();
+        }
+    }
+
+    void startRocket()
+    {
+        flying = true;
+        targetHeight = 100;
     }
 
     void FixedUpdate()
     {
         Debug.Log("rocket moving");
-        updatePosition();
-        this.transform.position = Vector3.up * (float)posY + Vector3.right * (float)posX;
+        if (flying)
+        {
+            updatePosition();
+            this.transform.position = Vector3.up * (float)posY + Vector3.right * (float)posX;
+            if(this.posY > targetHeight)
+            {
+                flying = false;
+            }
+        }
     }
 
     void updatePosition() {
