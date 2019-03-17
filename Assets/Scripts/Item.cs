3a4bb184bb95384bb84c8847f106f347c5fc4616
Severyn Kozak <severyn.kozak@gmail.com>
2019-03-17 15:58:36 -0400

    Fix stats null bug

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -20,7 +20,7 @@ public class Item
         this.description = description;
         this.icon = Resources.Load<Sprite>("Sprites/Items/" + iconName);
         this.stats = stats;
-        if (stats.ContainsKey("cost")){
+        if (stats != null && stats.ContainsKey("cost")){
             this.cost = stats["cost"];
         } else {
             this.cost = 0;
