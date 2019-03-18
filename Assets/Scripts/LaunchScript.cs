85780f649ee75c2e1f8378b7c4288692eff9919f
dotunade@mit.edu <dotunade@mit.edu>
2019-03-18 16:06:46 -0400

    Made Launch button not work unless rocket works

--- Assets/Scripts/LaunchScript.cs
+++ Assets/Scripts/LaunchScript.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LaunchScript : MonoBehaviour
 {
     private RocketController rocket;
+    public Button launchButton;
     // Start is called before the first frame update
     void Start()
     {
         rocket = FindObjectOfType<RocketController>();
+        this.launchButton.interactable = false;
         //this.GetComponent<>
+    }
+
+    private void Update()
+    {
+        if (this.CheckRocketParts())
+        {
+            Debug.Log("Does this work");
+            this.launchButton.interactable = true;
+        }
+            
     }
 
     public void onClickLaunch() 
@@ -18,6 +31,16 @@ public class LaunchScript : MonoBehaviour
         rocket.startRocket();
         this.gameObject.SetActive(false);
 
+    }
+
+    private bool CheckRocketParts()
+    {
+        bool hasFuel = this.rocket.getCurrentFuelType() != null;
+        bool hasTank = this.rocket.getCurrentFuelTank() != null;
+        bool hasConfig = this.rocket.getCurrentRocketConfiguration() != null;
+
+
+        return hasConfig && hasTank && hasFuel;
     }
 
 
